9c8cb1b91cdb5ed59f4f8d0f68fa496c433de192
Martin Björkström <martin.bjorkstrom@gmail.com>
2016-08-20 00:18:58 +0300

    Fabric, static files and websockets up

---
+++ WebApi/Controllers/ChatController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using Owin.WebSocket;
+
+namespace WebApi.Controllers
+{
+    public class ChatController : WebSocketConnection
+    {
+        public override Task OnMessageReceived(ArraySegment<byte> message, WebSocketMessageType type)
+        {
+            //Handle the message from the client
+
+            //Example of JSON serialization with the client
+            var json = Encoding.UTF8.GetString(message.Array, message.Offset, message.Count);
+
+            var toSend = Encoding.UTF8.GetBytes(json);
+
+            //Echo the message back to the client as text
+            return SendText(toSend, true);
+        }
+    }
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Http;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using Owin.WebSocket.Extensions;
+using WebApi.Controllers;
+
+namespace WebApi
+{
+    public static class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public static void ConfigureApp(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+
+            appBuilder.MapWebSocketRoute<ChatController>("/chat");
+
+            // Web APi
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            appBuilder.UseWebApi(config);
+
+            // Static files
+            var physicalFileSystem = new PhysicalFileSystem("wwwroot");
+            var options = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                FileSystem = physicalFileSystem
+            };
+            options.StaticFileOptions.FileSystem = physicalFileSystem;
+            options.StaticFileOptions.ServeUnknownFileTypes = true;
+            options.DefaultFilesOptions.DefaultFileNames = new[] { "index.html" };
+            appBuilder.UseFileServer(options);
+        }
+    }
+}
